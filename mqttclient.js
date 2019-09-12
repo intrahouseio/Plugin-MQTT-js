1dd968ec53611f04f203980ebb0475b444700892
nimagrolis <486016@gmail.com>
2019-09-12 15:49:25 +0300

    add act set

--- mqttclient.js
+++ mqttclient.js
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 2018 Intra LLC
  *
- * MIT LICENSE 
- * 
+ * MIT LICENSE
+ *
  * Mqtt client
  */
 
-const util = require("util");
-const path = require("path");
+const util = require('util');
+const path = require('path');
 
 /*
 const defaultPluginParams = {
@@ -18,35 +18,34 @@ const defaultPluginParams = {
 */
 
 // Standard IH plugin
-const manifest = path.join(__dirname, "mqttclient.json");
-const plugin = require("./lib/plugin").Plugin(manifest);
-
+const manifest = path.join(__dirname, 'mqttclient.json');
+const plugin = require('./lib/plugin').Plugin(manifest);
 
 // Wraps a client connection to an MQTT broker
-const agent = require("./lib/agent").Agent();
+const agent = require('./lib/agent').Agent();
 
 // Converts incoming and outgoing messages
-const converter = require("./lib/converter");
+const converter = require('./lib/converter');
 
-plugin.log("Mqtt client has started.",0);
-plugin.getFromServer("params");
+plugin.log('Mqtt client has started.', 0);
+plugin.getFromServer('params');
 
 /* Plugin event listeners */
-plugin.on("params", () => {
-  plugin.getFromServer("config");
-  plugin.getFromServer("extra");
+plugin.on('params', () => {
+  plugin.getFromServer('config');
+  plugin.getFromServer('extra');
 
   // Можно соединиться с брокером
   agent.connect(plugin.params);
 });
 
 // Каналы для получения данных
-plugin.on("config", data => {
+plugin.on('config', data => {
   converter.createSubMap(data);
 });
 
 // Массив для публикации данных
-plugin.on("extra", data => {
+plugin.on('extra', data => {
   if (!data || !Array.isArray(data)) {
     // plugin.log("Publish settings (extra array) missing or invalid!");
     return;
@@ -55,11 +54,10 @@ plugin.on("extra", data => {
   // Нужно подписаться на сервере IH на эти устройства, получать их значения и передавать
   let filter = converter.saveExtraGetFilter(data);
   // let filter = formDeviceFilter(data);
-  if (filter)
-    plugin.sendToServer("sub", { id: "main", event: "devices", filter });
+  if (filter) plugin.sendToServer('sub', { id: 'main', event: 'devices', filter });
 });
 
-plugin.on("sub", data => {
+plugin.on('sub', data => {
   // Получили данные от сервера по подписке - отправить брокеру
   if (!data) return;
 
@@ -73,92 +71,93 @@ plugin.on("sub", data => {
   });
 });
 
-plugin.on("act", data => {
+plugin.on('act', data => {
   // Получили от сервера команду(ы) для устройства - отправить брокеру
   // Команда уже готова - там должен быть topic и message
   if (!data) return;
 
   data.forEach(item => {
     try {
-      if (item.topic) agent.publish(item.topic, item.message || "");
+      if (item.topic) {
+        if (item.act == 'set' && (!item.message || item.message == 'value')) this.message = String(item.value);
+        agent.publish(item.topic, item.message || '');
+      }
     } catch (e) {
       logError(e, `Publish topic ${item.topic} message ${item.message}`);
     }
   });
 });
 
-plugin.on("command", message => {
+plugin.on('command', message => {
   // Получили от сервера сообщение {type:'command', command:'publish', data:{topic, message}}
   if (!message) return;
   let data;
   switch (message.command) {
-    case "publish": // Команда на прямую публикацию
-      if (!message.data)
-        return plugin.log("Not found data: " + util.inspect(message));
-      if (typeof message.data != "object")
-        return plugin.log("Invalid data: " + util.inspect(message));
+    case 'publish': // Команда на прямую публикацию
+      if (!message.data) return plugin.log('Not found data: ' + util.inspect(message));
+      if (typeof message.data != 'object') return plugin.log('Invalid data: ' + util.inspect(message));
 
       data = !Array.isArray(message.data) ? [message.data] : message.data;
       data.forEach(item => {
         try {
-          if (item.topic) agent.publish(item.topic, item.message || "");
+          if (item.topic) agent.publish(item.topic, item.message || '');
         } catch (e) {
           logError(e, `Publish topic ${item.topic} message ${item.message}`);
         }
       });
       break;
     default:
-      plugin.log("Missing or invalid command! Expected command:publish!");
+      plugin.log('Missing or invalid command! Expected command:publish!');
   }
 });
 
-plugin.on("exit", () => {
+plugin.on('exit', () => {
   processExit(0);
 });
 
 /* Agent event listeners */
-agent.on("connect", () => {
-  plugin.log("Connected!!", 1);
+agent.on('connect', () => {
+  plugin.log('Connected!!', 1);
 
   // На каналы нужно подписаться - массив топиков для подписки - выделить темы
   let topics = converter.getSubMapTopics();
-  plugin.log("Subscribe on  " + util.inspect(topics), 1);
+  plugin.log('Subscribe on  ' + util.inspect(topics), 1);
   agent.subscribe(topics);
 });
 
-agent.on("offline", () => {
-  plugin.log("offline", 1);
+agent.on('offline', () => {
+  plugin.log('offline', 1);
 });
 
-agent.on("log", (text, level) => {
+agent.on('log', (text, level) => {
   plugin.log(text, level);
 });
 
-agent.on("data", (topic, message) => {
-  message =   message.toString();
-  plugin.log("GET: " + topic + " " + message, 2);
+agent.on('data', (topic, message) => {
+  message = message.toString();
+  plugin.log('GET: ' + topic + ' ' + message, 2);
 
   if (converter.startsceneMap.has(topic)) {
-    converter.startsceneMap.get(topic).forEach( item => {
-        process.send({ type: "startscene", id: item, arg:{topic, message} });
+    converter.startsceneMap.get(topic).forEach(item => {
+      process.send({ type: 'startscene', id: item, arg: { topic, message } });
     });
   }
 
   let data = converter.convertIncoming(topic, message);
-  if (data) plugin.sendToServer("data", data);
+  if (data) plugin.sendToServer('data', data);
 });
 
 // Фатальная ошибка - выход плагина
-agent.on("error", txt => {
+agent.on('error', txt => {
   processExit(1, txt);
 });
 
 /* Private functions */
-function logError(err, txt = "") {
-  plugin.log(txt + " ERROR! " + JSON.stringify(err));
+function logError(err, txt = '') {
+  plugin.log(txt + ' ERROR! ' + JSON.stringify(err));
 }
 
-function processExit(errcode = 0, txt = "") {
+function processExit(errcode = 0, txt = '') {
   //  Close connection
   agent.end();
 
